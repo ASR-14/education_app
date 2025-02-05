dd103874106e16b276b4a915dc9026b9febae5be
Anh Khoa <phamvuanhkhoacv@gmail.com>
2025-02-05 16:21:31 +0700

    AuthRemoteDataSourceImpl

---
+++ lib/core/utils/constants.dart
@@ -0,0 +1,2 @@
+const kDefaultAvatar =
+    'https://hocdohoacaptoc.com/wp-content/uploads/2022/02/avata-dep-nam-2.jpg';
--- lib/src/auth/data/datasources/auth_remote_data_source.dart
+++ lib/src/auth/data/datasources/auth_remote_data_source.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:education_app/core/enums/update_user.dart';
 import 'package:education_app/core/errors/exceptions.dart';
+import 'package:education_app/core/utils/constants.dart';
 import 'package:education_app/core/utils/typdefs.dart';
 import 'package:education_app/src/auth/data/models/user_model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -64,21 +68,158 @@ class AuthRemoteDataSourceImpl implements AuthRemoteDataSource {
   Future<LocalUserModel> signIn({
     required String email,
     required String password,
-  }) async {}
+  }) async {
+    try {
+      final result = await _authClient.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      final user = result.user;
+
+      if (user == null) {
+        throw const ServerException(
+          message: 'Please try again later',
+          statusCode: 505,
+        );
+      }
+
+      var userData = await _getUserData(user.uid);
+
+      if (userData.exists) {
+        return LocalUserModel.fromMap(userData.data()!);
+      }
+
+      await _setUserData(user, email);
+
+      userData = await _getUserData(user.uid);
+      return LocalUserModel.fromMap(userData.data()!);
+    } on FirebaseAuthException catch (e) {
+      throw ServerException(
+        message: e.message ?? 'Error Occerred',
+        statusCode: 505,
+      );
+    } on ServerException {
+      rethrow;
+    } catch (e, s) {
+      debugPrintStack(stackTrace: s);
+      throw ServerException(
+        message: e.toString(),
+        statusCode: 505,
+      );
+    }
+  }
 
   @override
   Future<void> signUp({
     required String email,
     required String fullName,
     required String password,
-  }) async {}
+  }) async {
+    try {
+      final userCred = await _authClient.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      await userCred.user?.updateDisplayName(fullName);
+      await userCred.user?.updatePhotoURL(kDefaultAvatar);
+      await _setUserData(_authClient.currentUser!, email);
+    } on FirebaseAuthException catch (e) {
+      throw ServerException(
+        message: e.message ?? 'Error Occerred',
+        statusCode: 505,
+      );
+    } on ServerException {
+      rethrow;
+    } catch (e, s) {
+      debugPrintStack(stackTrace: s);
+      throw ServerException(
+        message: e.toString(),
+        statusCode: 505,
+      );
+    }
+  }
 
   @override
   Future<void> updateUser({
     required UpdateUserAction action,
     dynamic userData,
-  }) async {}
+  }) async {
+    try {
+      switch (action) {
+        case UpdateUserAction.email:
+          await _authClient.currentUser
+              ?.verifyBeforeUpdateEmail(userData as String);
+          await _updateUserData({'email': userData});
+        case UpdateUserAction.displayName:
+          await _authClient.currentUser?.updateDisplayName(userData as String);
+          await _updateUserData({'fullName': userData});
+        case UpdateUserAction.profilePic:
+          final ref = _dbClient
+              .ref()
+              .child('profile_pics/${_authClient.currentUser?.uid}');
 
+          await ref.putFile(userData as File);
+          final url = await ref.getDownloadURL();
+          await _authClient.currentUser?.updatePhotoURL(url);
+          await _updateUserData({'profilePic': url});
 
-  Future<DocumentSnapshot<DataMap>> _getUserData(String)
+        case UpdateUserAction.password:
+          if (_authClient.currentUser?.email == null) {
+            throw const ServerException(
+              message: 'User does not exist',
+              statusCode: 505,
+            );
+          }
+          final newData = jsonDecode(userData as String) as DataMap;
+          await _authClient.currentUser?.reauthenticateWithCredential(
+            EmailAuthProvider.credential(
+              email: _authClient.currentUser!.email!,
+              password: newData['oldPassword'] as String,
+            ),
+          );
+
+          await _authClient.currentUser?.updatePassword(
+            newData['newPassword'] as String,
+          );
+
+        case UpdateUserAction.bio:
+          await _updateUserData({'bio': userData as String});
+      }
+    } on FirebaseException catch (e) {
+      throw ServerException(
+        message: e.message ?? 'Error Occerred',
+        statusCode: 505,
+      );
+    } catch (e, s) {
+      debugPrintStack(stackTrace: s);
+      throw ServerException(
+        message: e.toString(),
+        statusCode: 505,
+      );
+    }
+  }
+
+  Future<DocumentSnapshot<DataMap>> _getUserData(String uid) async {
+    return _cloudStoreClient.collection('users').doc(uid).get();
+  }
+
+  Future<void> _setUserData(User user, String fallbackEmail) async {
+    await _cloudStoreClient.collection('users').doc(user.uid).set(
+          LocalUserModel(
+            uid: user.uid,
+            email: user.email ?? fallbackEmail,
+            profilePic: user.photoURL ?? '',
+            points: 0,
+            fullName: user.displayName ?? '',
+          ).toMap(),
+        );
+  }
+
+  Future<void> _updateUserData(DataMap data) async {
+    await _cloudStoreClient
+        .collection('users')
+        .doc(_authClient.currentUser?.uid)
+        .update(data);
+  }
 }
