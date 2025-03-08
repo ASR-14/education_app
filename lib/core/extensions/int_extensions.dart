de4c6aefa7519d843de55efa602633070c750aa8
Anh Khoa <phamvuanhkhoacv@gmail.com>
2025-03-08 14:10:27 +0700

    update

--- lib/core/extensions/int_extensions.dart
+++ lib/core/extensions/int_extensions.dart
@@ -1,5 +1,19 @@
-import 'package:flutter/material.dart';
+// extension IntExtensions on int {
+//   SizedBox get sb => SizedBox(width: toDouble(), height: toDouble());
+// }
 
-extension IntExtensions on int {
-  SizedBox get sb => SizedBox(width: toDouble(), height: toDouble());
+// extension IntExt on int {
+//   String get estimate {
+//     if (this < 10) return '$this';
+//     return 'over ${this ~/ 10 * 10}';
+//   }
+// }
+
+extension IntExt on int {
+  String get estimate {
+    if (this <= 10) return '$this';
+    var data = this - (this % 10);
+    if (data == this) data = this - 5;
+    return 'over $data';
+  }
 }
