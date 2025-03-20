cdee52bb30ad933897b74c543923e5c9e73f05c7
Anh Khoa <phamvuanhkhoacv@gmail.com>
2025-03-20 16:53:29 +0700

    update

--- lib/src/profile/presentation/widgets/user_info_card.dart
+++ lib/src/profile/presentation/widgets/user_info_card.dart
@@ -10,12 +10,54 @@ class UserInfoCard extends StatelessWidget {
   });
 
   final Color infoThemeColour;
-  final dynamic infoIcon;
+  final Widget infoIcon;
   final String infoTitle;
   final String infoValue;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      height: 84,
+      width: 156,
+      decoration: BoxDecoration(
+        border: Border.all(color: const Color(0xFFE4E6EA)),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 40,
+              width: 40,
+              decoration: BoxDecoration(
+                color: infoThemeColour,
+                shape: BoxShape.circle,
+              ),
+              child: Center(child: infoIcon),
+            ),
+            const SizedBox(width: 10),
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Text(
+                  infoTitle,
+                  style: const TextStyle(fontSize: 12),
+                ),
+                Text(
+                  infoValue,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 18,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
