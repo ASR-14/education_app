6856e116609f1bfc8130964c5ed7120b91ba8379
Anh Khoa <phamvuanhkhoacv@gmail.com>
2025-03-25 12:09:53 +0700

    material

--- lib/core/usecases/usecases.dart
+++ lib/core/usecases/usecases.dart
@@ -11,3 +11,15 @@ abstract class UsecaseWithoutParams<Type> {
 
   ResultFuture<Type> call();
 }
+
+abstract class StreamUsecaseWithoutParams<Type> {
+  const StreamUsecaseWithoutParams();
+
+  ResultStream<Type> call();
+}
+
+abstract class StreamUsecaseWithParams<Type, Params> {
+  const StreamUsecaseWithParams();
+
+  ResultStream<Type> call(Params params);
+}
