c6f8501d6d1cf6181b4e71abf63fd7a18cdcea3a
Anh Khoa <phamvuanhkhoacv@gmail.com>
2025-04-12 14:59:08 +0700

    video UI

--- lib/src/home/presentation/refactors/home_body.dart
+++ lib/src/home/presentation/refactors/home_body.dart
@@ -6,6 +6,7 @@ import 'package:education_app/core/utils/core_utils.dart';
 import 'package:education_app/src/course/presentation/cubit/course_cubit.dart';
 import 'package:education_app/src/home/presentation/refactors/home_header.dart';
 import 'package:education_app/src/home/presentation/refactors/home_subjects.dart';
+import 'package:education_app/src/home/presentation/refactors/home_videos.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -30,6 +31,17 @@ class _HomeBodyState extends State<HomeBody> {
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<CourseCubit, CourseState>(
+      listener: (_, state) {
+        if (state is CourseError) {
+          CoreUtils.showSnackBar(context, state.message);
+        } else if (state is CoursesLoaded && state.courses.isNotEmpty) {
+          final courses = state.courses..shuffle();
+          final courseOfTheDay = courses.first;
+          context
+              .read<CourseOfTheDayNotifier>()
+              .setCourseOfTheDay(courseOfTheDay);
+        }
+      },
       builder: (context, state) {
         if (state is LoadingCourses) {
           return const LoadingView();
@@ -51,21 +63,12 @@ class _HomeBodyState extends State<HomeBody> {
               const HomeHeader(),
               const SizedBox(height: 20),
               HomeSubjects(courses: courses),
+              const SizedBox(height: 20),
+              const HomeVideos(),
             ],
           );
         }
         return const SizedBox.shrink();
-      },
-      listener: (_, state) {
-        if (state is CourseError) {
-          CoreUtils.showSnackBar(context, state.message);
-        } else if (state is CoursesLoaded && state.courses.isNotEmpty) {
-          final courses = state.courses..shuffle();
-          final courseOfTheDay = courses.first;
-          context
-              .read<CourseOfTheDayNotifier>()
-              .setCourseOfTheDay(courseOfTheDay);
-        }
       },
     );
   }
